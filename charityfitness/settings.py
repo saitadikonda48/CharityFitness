653c08207d0d2fd16932a0c8d7466f23979c8f64
SaiTadikonda97 <saitadikonda19@gmail.com>
2016-06-27 22:10:49 -0400

    Added Website

--- charityfitness/settings.py
+++ charityfitness/settings.py
@@ -139,7 +139,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
-# Update database configuration with $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# # Update database configuration with $DATABASE_URL.
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
