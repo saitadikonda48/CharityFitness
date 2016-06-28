dcbec51b1ac48a353d92da5cb9d47bacecd08002
SaiTadikonda97 <saitadikonda19@gmail.com>
2016-06-27 21:26:22 -0400

    Added Website

--- charityfitness/settings.py
+++ charityfitness/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'charityfitness.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
