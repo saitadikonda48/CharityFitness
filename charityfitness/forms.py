af674c31e6176666199abd778fbdce7cea72b186
Sai Tadikonda <saitadikonda48@gmail.com>
2017-01-30 16:49:01 -0500

    Remove files

--- charityfitness/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class WeightForm(forms.Form):
-    your_weight = forms.FloatField(label='', 
-    	widget=forms.TextInput(attrs={'placeholder': 'Enter Today\'s Weight'}))
-
-class CalorieForm(forms.Form):
-	caloric_intake = forms.IntegerField(label='',
-		widget=forms.TextInput(attrs={'placeholder': 'Enter Caloric Intake'}))
